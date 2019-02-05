b41a4f4c14a94393c5486a5a74dd17f7946992c5
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2019-02-05 09:30:26 +0530

    problem 25

--- level-one/problem22.go
+++ level-one/problem22.go
@@ -1,1 +1,108 @@
 package levelone
+
+import (
+	"fmt"
+	//"strconv"
+)
+
+var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var nameMap = make(map[string][]string)
+var alphaIntMap = make(map[string]int)
+
+func initAlphaMap() {
+	for i, a := range alpha {
+		v := i + 1
+		alphaIntMap[string(a)] = v
+	}
+}
+
+func CalculateNameScores(namesList []string) int {
+	if len(alphaIntMap) == 0 {
+		initAlphaMap()
+	}
+	for _, n := range namesList {
+		SortName(n)
+	}
+
+	f := []string{}
+	for _, a := range alpha {
+		val := nameMap[string(a)]
+		f = append(f, val...)
+	}
+
+	score := calculateScore(f)
+	fmt.Printf("Score for %d names is :  %d\n", len(f), score)
+	return score
+
+}
+
+func calculateScore(sNames []string) int {
+	if len(alphaIntMap) == 0 {
+		initAlphaMap()
+	}
+	score := 0
+	for i, n := range sNames {
+		ns := 0
+		for _, a := range n {
+			v := alphaIntMap[string(a)]
+			ns += v
+		}
+		score += (i + 1) * ns
+	}
+	return score
+}
+
+func SortName(name string) {
+	f := string(name[0])
+	if nameMap[f] == nil {
+		val := []string{name}
+		nameMap[f] = val
+	} else {
+		placed := false
+		arr := nameMap[f]
+		for r, n := range arr {
+			cmp := StrCompare(name, n)
+			if cmp <= 0 {
+				placed = true
+				if r == 0 {
+					arr = append([]string{name}, arr...)
+				} else {
+					arr = append(arr[0:r], append([]string{name}, arr[r:]...)...)
+				}
+				break
+			}
+		}
+		if !placed {
+			arr = append(arr, []string{name}...)
+		}
+		nameMap[f] = arr
+	}
+}
+
+func StrCompare(a, b string) int {
+	la := len(a)
+	lb := len(b)
+	if len(alphaIntMap) == 0 {
+		initAlphaMap()
+	}
+	i := 0
+	for ; i < la; i++ {
+		if i < lb {
+			ia := alphaIntMap[string(a[i])]
+			ib := alphaIntMap[string(b[i])]
+			if ia > ib {
+				return 1
+			} else if ia == ib {
+				continue
+			} else {
+				return -1
+			}
+		} else if i < la {
+			return 1
+		}
+	}
+	if i == la && i < lb {
+		return -1
+	}
+	return 0
+}
--- level-one/problem22_test.go
+++ level-one/problem22_test.go
@@ -1,69 +1,68 @@
 package levelone
 
-import "strconv"
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
 
-var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var nameMap = make(map[string][]string)
-var alphaIntMap = make(map[string]int)
-
-func CalculateNameScores(namesList []stirng) int {
-	for _, a := range alpha {
-		v := strconv.Atoi(a)
-		alphaIntMap[string(a)] = v
+func TestStrCompare(t *testing.T) {
+	namePairs := []struct {
+		a string
+		b string
+		r int
+	}{
+		{"COLIN", "COLINS", -1},
+		{"ABE", "BEN", -1},
+		{"BEN", "ABE", 1},
+		{"BEN", "BEN", 0},
+		{"ZACH", "XMEN", 1},
+		{"BEN", "BOON", -1},
+		{"BUSTER", "BEN", 1},
+		{"BUSTER", "BOON", 1},
 	}
 
-	for _, n := range nameList {
+	for _, c := range namePairs {
+		r := StrCompare(c.a, c.b)
+		if r != c.r {
+			t.Errorf("For pair (%s, %s) expected the result to be %d but got %d", c.a, c.b, c.r, r)
+		}
+	}
+}
+
+func TestSortName(t *testing.T) {
+	names := []string{"ABE", "BUSTER", "COLINS", "CABE", "ANTMAN", "ZACH", "JIM", "BEN", "BOON", "ZAP"}
+
+	for _, n := range names {
 		SortName(n)
 	}
+	t.Logf("Sorted Map %v\n", nameMap)
 
 }
 
-func SortName(name string) {
-	f := string(name[0])
-	if nameMap[f] == nil {
-		val := []string{name}
-		nameMap[f] = val
-	} else {
-		arr := nameMap[f]
-		for r, n := range arr {
-			cmp := StrCompare(name, n)
-			if cmp <= 0 {
-				if r == 0 {
-					arr = append([]string{name}, arr...)
-				} else {
-					t := append(arr[0:r], []string{name})
-					arr = append(t, arr[r+1:len(arr)-1])
-				}
-				break
-			}
-		}
-		arr = append(arr, []string{name})
-
-		nameMap[f] = arr
+func TestCalcScore(t *testing.T) {
+	n := []string{"COLIN"}
+	v := 53
+	e := calculateScore(n)
+	if v != e {
+		t.Errorf("Expected value %d but got %d", v, e)
 	}
 }
 
-func StrCompare(a, b string) int {
-	la := len(a)
-	lb := len(b)
-
-	for i := 0; i < la; i++ {
-		if i < lb {
-			ia := alphaIntMap[string(a[i])]
-			ib := alphaIntMap[string(b[i])]
-			if ia > ib {
-				return 1
-			} else if ia == ib {
-				return 0
-			} else {
-				return -1
-			}
-		} else if i < la {
-			return 1
-		}
-		if i == la && i < lb {
-			return -1
-		}
-		return 0
+func TestNameScores(t *testing.T) {
+	dat, err := ioutil.ReadFile("p022_names.txt")
+	if err != nil {
+		t.Errorf("Error Reading input file %s", err.Error())
 	}
+	s := string(dat)
+	s = s[1:]
+	s = s[:len(s)-1]
+	t.Logf("Length of string from file %d", len(s))
+	namesList := strings.Split(s, "\",\"")
+	if namesList != nil && len(namesList) == 0 {
+		t.Errorf("Error cannot split the names")
+	}
+	t.Logf("Number of names %d", len(namesList))
+
+	CalculateNameScores(namesList)
 }
